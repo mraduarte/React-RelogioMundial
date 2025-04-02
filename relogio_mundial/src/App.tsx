dff07bd335a5cdd7a770cfa711b282b02547e960
mraduarte <mcduarte9@hotmail.com>
2025-04-02 20:00:31 -0300

    Setup inicial

---
+++ relogio_mundial/src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <div>
+      <h1>Relógio Mundial</h1>
+      <select>
+        <option value="" disabled selected>
+          Selecione um fuso horário
+        </option>
+      </select>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
---
+++ relogio_mundial/src/components/TimeZoneClock.tsx
@@ -0,0 +1,8 @@
+const TimeZoneClock = () => {
+  return <div>
+    <h2>timezone</h2>
+    <h3>tempo</h3>
+  </div>;
+};
+
+export default TimeZoneClock;
